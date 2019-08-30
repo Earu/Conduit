7fb40748927230b429787596f1276a13271d1fb0
Earu <earu.derg@gmail.com>
2019-08-30 12:56:13 +0200

    Current work on routing

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,9 +8,21 @@ export class Login extends React.Component<ConduitProps, {}> {
         let input: HTMLInputElement = document.getElementById('token-input') as HTMLInputElement;
         if (input.value) {
             let form = document.getElementById('token-form');
+            let loader = document.getElementById('loader');
+            loader.style.display = 'block';
+            input.disabled = true;
             this.props.client.login(input.value)
-                .then(_ => form.style.display = 'none')
-                .catch(console.error);
+                .then(_ => {
+                    form.style.display = 'none';
+                    loader.style.display = 'none';
+                    input.disabled = false;
+                })
+                .catch(err => {
+                    input.style.border = '2px solid red';
+                    loader.style.display = 'none';
+                    input.disabled = false;
+                    console.error(err);
+                });
         } else {
             input.style.border = '2px solid red';
         }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Discord from 'discord.js';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history'
 
 const client = new Discord.Client();
 client.on('ready', () => {
@@ -10,8 +12,17 @@ client.on('ready', () => {
 client.on('message', console.log);
 
 import { Login } from './components/login';
+import { Loader } from './components/loader';
+import { Dashboard } from './components/dashboard';
 
 ReactDOM.render(
-    <Login client={client} />,
+    <div>
+        <Router history={createBrowserHistory()}>
+            <Route path='/' exact><Login client={client} /></Route>
+            <Route path='/login'><Login client={client} /></Route>
+            <Route path='dashboard'><Dashboard client={client} /></Route>
+        </Router>
+        <Loader />
+    </div>,
     document.getElementById('root')
 );
